9987189826b4f82ed4d7cf7807fb4d69ad4970f0
Fernando Velasco Salazar <fernandovs4@gmail.com>
2020-03-05 13:06:19 -0600

    YA corre

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	    try{
 	        BankAccountsFileReader reader=new BankAccountsFileReader(new File("/Users/fernando/Downloads/OCR/src/com/company/BankAccounts.txt"));
-            reader.readAccountFile();
+            reader.getAccounts();
         }catch (Exception ex){
             ex.printStackTrace();
         }
